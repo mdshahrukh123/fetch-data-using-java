8f225617bce7dcfd969f599fff64ab82229cf419
shahrukh <mohdshahrukhmzn1@gmail.com>
2023-04-13 01:22:38 +0530

    first commit

---
+++ src/main/java/org/example/demo.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class demo {
+    public static void main(String []args) throws Exception{
+        Object https;
+        var url="https://api.chucknorris.io/jokes/random";
+        var request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+       var client= HttpClient.newBuilder().build();
+       var response = client.send(request, HttpResponse.BodyHandlers.ofString());
+      System.out.println(response.statusCode());
+       System.out.println(response.body());
+    }
+}
