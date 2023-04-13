699db55c9225c1363379fa16d68dfac937e2c2cf
shahrukh <mohdshahrukhmzn1@gmail.com>
2023-04-13 13:53:45 +0530

    commit

--- src/main/java/org/example/demo.java
+++ src/main/java/org/example/demo.java
@@ -8,7 +8,7 @@ import java.net.http.HttpResponse;
 public class demo {
     public static void main(String []args) throws Exception{
         Object https;
-        var url="https://api.chucknorris.io/jokes/random";
+        var url="https://api.nationalize.io/?name=nathaniel";
         var request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
        var client= HttpClient.newBuilder().build();
        var response = client.send(request, HttpResponse.BodyHandlers.ofString());
